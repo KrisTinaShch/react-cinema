2fdf9a64ab14fb1e0b0cab4e33cdfcbac9fd82d6
Kris <kris.shcheglova@bk.ru>
2025-03-01 21:44:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Layout/Header'
+import Footer from './components/Layout/Footer'
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
