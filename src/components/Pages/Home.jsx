3f4c1673e6f8f7dcf326531452c8a9200e53a46b
Kris <kris.shcheglova@bk.ru>
2025-03-03 01:40:42 +0200

    create Home jsx structure

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,5 +1,21 @@
-// function Home() {
-//     return()
-// }
 
-// export default Home;
+import Banner from '../Home/Banner'
+import Filter from '../Home/Filter'
+import MoviesList from '../Movies/MoviesList'
+
+function Home() {
+    return (
+        <>
+            <Banner />
+            <div className="container">
+                <Filter />
+                <MoviesList />
+                <div className="movies__scroll">
+                    <button><i className="fa-solid fa-arrow-down"></i></button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
