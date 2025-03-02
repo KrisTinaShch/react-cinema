fccf69e2c4fa4038940f7ca8f9067a735ad6aa2a
Kris <kris.shcheglova@bk.ru>
2025-03-02 23:37:33 +0200

    change filter naming

--- src/components/Home/Filter.jsx
+++ src/components/Home/Filter.jsx
@@ -1,25 +1,19 @@
 
 function Filter() {
+    let filter__categoryes = ["Popular", "Novelty", "Featured", "Short films"]
     return (
         <div className="filter">
             <div id="categoryes" className="filter__categoryes anchor">
                 <div className="filter__categoryes-flex">
-                    <div onClick="changeActiveCategorySort(this)" className="filter__categoryes-item active-category">
-                        <p>Popular</p>
-                        <span></span>
-                    </div>
-                    <div onClick="changeActiveCategorySort(this)" className="filter__categoryes-item">
-                        <p>Novelty</p>
-                        <span></span>
-                    </div>
-                    <div onClick="changeActiveCategorySort(this)" className="filter__categoryes-item">
-                        <p>Featured</p>
-                        <span></span>
-                    </div>
-                    <div onClick="changeActiveCategorySort(this)" className="filter__categoryes-item">
-                        <p>Short films</p>
-                        <span></span>
-                    </div>
+                    {
+                        filter__categoryes.map((value, index) =>
+                        (
+                            <div onClick="changeActiveCategorySort(this)" className="filter__categoryes-item active-category" key={index}>
+                                <p>{value}</p>
+                                <span></span>
+                            </div>
+                        ))
+                    }
                 </div>
                 <span></span>
             </div>
