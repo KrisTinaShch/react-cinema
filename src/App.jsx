8e1220bffe7dfc495db02fc5d341cde857ff0f03
Kris <kris.shcheglova@bk.ru>
2025-03-02 15:49:16 +0200

    create new Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Layout/Header'
 import Footer from './components/Layout/Footer'
+import Banner from './components/Home/Banner'
 function App() {
 
   return (
     <>
       <Header />
+      <Banner/>
       <Footer />
     </>
   )
